c39601f83edc4e35ae2c000f8d7f4c0ee416e9a0
futnucks <futnucks@gmail.com>
2011-07-21 23:07:42 -0500

    + switched to old menu by default + improved AntTweakBar loading, it shouldn't ever crash now during the game, but   it does crash when exiting the game. + version moved to v4.2.0.1 in preperation for this mainly bug fix release.

--- src/main.h
+++ src/main.h
@@ -28,7 +28,7 @@
 
 
 #define M0D_NAME		"mod_sa"
-#define M0D_VERSION		"4.3.0.0"
+#define M0D_VERSION		"4.3.0.1"
 #define SAMP_VERSION	"SA:MP 0.3cR3"
 
 // Use this to activate the SQLite Vehicle Recording.
